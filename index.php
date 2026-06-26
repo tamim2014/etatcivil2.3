577a0b758a38d12bb01c8dc2320ac7fae57dcd2f
unknown <a.andjib@yahoo.fr>
2026-06-26 23:36:08 +0200

    Beug color textarea resolu!

--- index.php
+++ index.php
@@ -20,33 +20,14 @@
 	    .t_area{
 			    /* padding-top:1.1em; */
 			    background-color: white;
-				color:#000 !important;
-				/* virer la poignée de redimensionnement 
-				resize:none;
-				*/
 				
-				/* Template creux - master handi: Sans box-shadow 				
-					border: 2px solid gray;
-					border-bottom: 0;
-					border-right: 0;
-				*/
-				
-				/* Template  box-shadow 
-				   box-shadow :inset 0 2px 4px rgba(0,0,0,.15);
-				*/
-				
-							
-				/* ********** Template Appli CORAN: OK pour un conteneur sombre **********************  
-				    box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.5) inset, 
-					0px 2px 2px 0px rgba(255, 255, 255, 0.5);
-                    border: 1px solid #8c8b8b; 
+				color:#000 !important;				
+				-webkit-text-fill-color: red !important;
+				/* 
+				  virer la poignée de redimensionnement 
+				   resize:none;
 				*/
 
-                /* ********** Template creux sobre **********************  
-				 box-shadow :
-				         inset 0 2px 4px rgba(0,0,0,.12),
-				         inset 0 -1px 1px rgba(255,255,255,.5);				
-                */
 		}
         @media (max-width: 768px) {
             .t_area{
@@ -54,6 +35,17 @@
                 font-size:0.82rem !important;
                 color:#000 !important;
             }
+			.t_area {
+				background:#fff !important;
+				color:#000 !important;
+				-webkit-text-fill-color:#000 !important;
+
+				/* Améliore la netteté sur Android */
+				text-rendering: optimizeLegibility;
+				-webkit-font-smoothing: antialiased;
+				font-weight: 500; /* léger renforcement pour compenser le flou */
+			}
+
         }
 	</style>
 
@@ -91,7 +83,8 @@
 						 <tr><td> <font color="#cdbe9f"><b>Entrer votre</b></font> login<br/> <input type="text"   id="login_"  name="pseudo_" > </td></tr> 
 						 <tr><td> <font color="#cdbe9f"><b>Votre</b></font> mot de passe<br/> <input type="password"  id="pswd_"   name="motdepasse_"> </td></tr>
 						 <tr ><td style="padding-top:1em;">
-							 <textarea  class="t_area" style="color:#000 !important;"> <?php echo $message; ?> </textarea>
+							<!-- <textarea  class="t_area" style="color:#000 !important;"> <"?"php echo $message; ?> </textarea> -->
+							 <textarea id="t_area" name="myTextBox" class="t_area" style="color:#000 !important;"></textarea>
 						 <br/><input id="valider_" type="submit" class="submit btnHover" value="Valider"   name="envoie"/>
 						 </td></tr>
 					</table>
@@ -112,6 +105,31 @@
 			<span>Etat civil</span>
 		</p>
     </div>
+	
+	
+    <!-- Pour pouvoir mettre le color du textarea en noir sur mobil - car il est consideré comme un placeholder   -->
+	<script>
+	document.addEventListener("DOMContentLoaded", function() {
+
+		const t = document.getElementById('t_area');
+
+		// Injecter le texte APRÈS le chargement
+		t.value = <?php echo json_encode($message); ?>;
+
+		// Forcer Android à recalculer le style
+		t.style.display = 'none';
+		void t.offsetHeight;
+		t.style.display = '';
+
+		// Reforcer la couleur
+		t.style.webkitTextFillColor = "#000";
+	});
+	</script>
+
+
+
+
+
 
 </body>
 </html>
